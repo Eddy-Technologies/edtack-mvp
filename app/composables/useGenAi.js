3ed48b102109b2d52b18f58221babd1f5122d70b
Johann Wong Jun Guo <johannwjg@gmail.com>
2025-01-12 10:48:17 +0800

    Update useGenAi.js

--- app/composables/useGenAi.js
+++ app/composables/useGenAi.js
@@ -1,7 +1,7 @@
 import {GoogleGenerativeAI} from '@google/generative-ai'
 
 export const useGenAi = async (modelType, schema) => {
-    const VITE_GOOGLE_AI_STUDIO_API_KEY = import.meta.env.VITE_GOOGLE_AI_STUDIO_API_KEY
+    const VITE_GOOGLE_AI_STUDIO_API_KEY = process.env.VITE_GOOGLE_AI_STUDIO_API_KEY;
 
     const genAI = new GoogleGenerativeAI(VITE_GOOGLE_AI_STUDIO_API_KEY)
     return genAI.getGenerativeModel({
