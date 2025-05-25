6e3e9fb35d9ce27edb9405227d235a532b095ac8
Fyelight <kaid.fyelight@hotmail.com>
2025-05-25 13:17:23 +0800

    ADD register logout

--- server/api/app-auth/logout.post.ts
+++ server/api/app-auth/logout.post.ts
@@ -1,12 +1,12 @@
-// export default defineEventHandler(async (event) => {
-//   // Clear the HttpOnly cookie by setting its expiry to a past date
-//   setCookie(event, 'app_user_jwt', '', {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === 'production',
-//     maxAge: 0, // Set maxAge to 0 to immediately expire the cookie
-//     path: '/',
-//     sameSite: 'lax',
-//   });
+export default defineEventHandler(async (event) => {
+  // Clear the HttpOnly cookie by setting its expiry to a past date
+  setCookie(event, 'app_user_jwt', '', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 0, // Set maxAge to 0 to immediately expire the cookie
+    path: '/',
+    sameSite: 'lax',
+  });
 
-//   return { message: 'App user logged out successfully.' };
-// });
+  return { message: 'App user logged out successfully.' };
+});
