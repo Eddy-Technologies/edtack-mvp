a7169302f396171be7b562979bdc662cc2c6ee8d
Jeremy <jeremythng.dev@gmail.com>
2025-07-24 16:05:44 +0700

    config

--- server/utils/stripe.ts
+++ server/utils/stripe.ts
@@ -6,11 +6,11 @@ export function getStripe(): Stripe {
   if (!stripeInstance) {
     const config = useRuntimeConfig();
 
-    if (!config.stripeSecretKey) {
+    if (!config.private.stripeSecretKey) {
       throw new Error('Stripe secret key is not configured. Please set NUXT_STRIPE_SECRET_KEY in your environment variables.');
     }
 
-    stripeInstance = new Stripe(config.stripeSecretKey, {
+    stripeInstance = new Stripe(config.private.stripeSecretKey, {
       typescript: true,
     });
   }
