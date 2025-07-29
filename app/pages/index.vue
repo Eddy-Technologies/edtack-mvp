4b1d8f558980e10b72f95d48f7270f1d2c0fe1cc
Jeremy <jeremythng.dev@gmail.com>
2025-07-29 18:41:17 +0700

    clean up

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,22 +8,9 @@
 </template>
 
 <script>
-import { useCreditStore } from '~/stores/credit'; // Import your store
-import { useProfileStore } from '~/stores/profile';
-
 export default {
   setup() {
-    const credits = ref(0);
-    const creditStore = useCreditStore();
-    const profileStore = useProfileStore();
-    const isParent = profileStore.profile === '/parent';
-    onMounted(() => {
-      credits.value = isParent ? creditStore.parentCredits : creditStore.childCredits[0];
-    });
-
-    return {
-      credits,
-    };
+    // Any setup logic can go here
   },
 };
 </script>
