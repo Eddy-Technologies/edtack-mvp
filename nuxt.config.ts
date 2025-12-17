ddf74fcff5ef34b2143b395edf4daa14632d0cca
Fyelight <kaid.fyelight@hotmail.com>
2025-12-17 09:11:04 +0800

    Update .gitignore with vercel and env files and nuxt.config.ts with compatability date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
+  compatibilityDate: '2025-12-15',
 
   future: { compatibilityVersion: 4 },
 
