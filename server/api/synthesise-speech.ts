79b0a4d2c737e8fc805e704989c6323ba60787f1
Jeremy <jeremythng.dev@gmail.com>
2025-12-17 21:51:26 +0700

    delete unused files

--- server/api/synthesise-speech.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler, createError } from 'h3';
-
-export default defineEventHandler(() => {
-  throw createError({
-    statusCode: 501,
-    message: 'Text-to-speech feature is not implemented.',
-  });
-});
