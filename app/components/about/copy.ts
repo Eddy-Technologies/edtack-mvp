c6364e34a738d45824026832aae0881409d4c12d
Jeremy <jeremythng.dev@gmail.com>
2026-02-03 17:58:30 +0700

    minor copy

--- app/components/about/copy.ts
+++ app/components/about/copy.ts
@@ -2,19 +2,19 @@ export const copy = {
   keyFeatures: [
     {
       title: 'O Level Preparation',
-      content: 'Using data from MOE syllabuses and content to generate tailored learning materials'
+      content: 'Vetted content used to generate tailored learning materials'
 
     },
     {
       title: '24/7 access, safe and instant feedback',
-      content: 'Automated marking and instant personalized feedback to accelerate learning. Guardrails in place to ensure safe usage.'
+      content: 'Automated marking and instant personalized feedback to accelerate learning.'
     },
     {
       title: 'Not just another "ChatGPT"',
-      description: ['Personalised lessons', 'Adaptive quizzes', 'Credit Reward system', 'Instant Marking and Feedback']
+      description: ['Personalised lessons', 'Adaptive quizzes', 'Credit Reward system', 'Instant Marking and Feedback', 'Guardrails in place to ensure safe usage.']
     },
     {
-      title: 'Parent Dashboard',
+      title: 'Parents are part of the journey',
       content: 'Track your child\'s progress, manage rewards, and stay involved in their learning journey.'
     }
   ]
